f947cb8e237b21a7d9655f19571522c2f4ccd271
EdgarZamoraa <zamorasaucedoedgar@gmail.com>
2020-05-26 02:06:24 -0500

    se agregaron los property a la clase empleado

--- empleado.py
+++ empleado.py
@@ -2,4 +2,27 @@ class Empleado:
     def __init__ (self,idempleado,nombre,direccion):
         self.__idempleado, self.__nombre, self.__direccion = idempleado, nombre, direccion
 
+    @property 
+    def idempleado(self):
+        return self.__idempleado
+
+    @property
+    def nombre(self):
+        return self.__nombre
+    @nombre.setter
+    def nombre(self,valor):
+        self.__nombre = valor
     
+    @property
+    def nombre(self):
+        return self.__nombre
+    @nombre.setter
+    def nombre(self,valor):
+        self.__nombre = valor
+    
+    @property
+    def direccion(self):
+        return self.__direccion
+    @direccion
+    def direccion(self,valor):
+        self.__nombre = valor
