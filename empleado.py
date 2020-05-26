013de451431371d9eee003dc19c4aeb0391b9298
EdgarZamoraa <zamorasaucedoedgar@gmail.com>
2020-05-26 14:29:34 -0500

    pruebas de open

--- empleado.py
+++ empleado.py
@@ -5,24 +5,28 @@ class Empleado:
     @property 
     def idempleado(self):
         return self.__idempleado
-
     @property
     def nombre(self):
         return self.__nombre
     @nombre.setter
     def nombre(self,valor):
         self.__nombre = valor
-    
-    @property
-    def nombre(self):
-        return self.__nombre
-    @nombre.setter
-    def nombre(self,valor):
-        self.__nombre = valor
-    
     @property
     def direccion(self):
         return self.__direccion
-    @direccion
+    @direccion.setter
     def direccion(self,valor):
         self.__nombre = valor
+
+
+
+ArchEmple = open("./archivos/Empleado.txt","a",encoding="utf8")
+
+Lineas = ArchEmple.readlines()
+Lineas[3] = "esta es la linea 4 indice 3"
+
+
+
+ArchEmple.close()
+        
+
