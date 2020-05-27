f76941dd564554a8dd066ca08ba05eea853f21d0
EdgarZamoraa <zamorasaucedoedgar@gmail.com>
2020-05-27 18:06:16 -0500

    mi version final

--- empleado.py
+++ empleado.py
@@ -6,39 +6,43 @@ def limpiar():
         system("clear")
 
 def EmpleadoVigente():
-        ArchivoEmpleado = open("./Archivos/Empleado.txt","r",encoding="utf8")
-        ArchivoEB = open("./archivos/EmpleadoBorrado.txt","r",encoding="utf8")
-        ListaEmpleados = (ArchivoEmpleado.read().splitlines())
-        ListaBorrados = (ArchivoEB.read().splitlines())
-        ListaEmpleadosVigentes = []
-        IDEmpleados = []
-        for E in ListaEmpleados:
-            N = (E[0:1])
-            IDEmpleados.append(N)
+    ##SE CREA LA COMPARACION DE EMPLEADO Y EMPLEADO BORRADO##
+    ArchivoEmpleado = open("./Archivos/Empleado.txt","r",encoding="utf8")
+    ArchivoEB = open("./archivos/EmpleadoBorrado.txt","r",encoding="utf8")
+    ListaEmpleados = (ArchivoEmpleado.read().splitlines())
+    ListaBorrados = (ArchivoEB.read().splitlines())
+    ListaEmpleadosVigentes = []
+    IDEmpleados = []
+    for E in ListaEmpleados:
+        A = (E.split("|"))
+        N = A[0]
+        IDEmpleados.append(N)
 
-        IDEmpleadoSET = set(IDEmpleados)
-        ListaEmpleadosSET = set(ListaBorrados)
+    IDEmpleadoSET = set(IDEmpleados)
+    ListaEmpleadosSET = set(ListaBorrados)
 
-        final = IDEmpleadoSET - ListaEmpleadosSET
-        FINAL = list(final)
+    final = IDEmpleadoSET - ListaEmpleadosSET
+    FINAL = list(final)
 
-        for E in ListaEmpleados:
-            N = (E[0:1])
-            for S in FINAL:
-                if N == S:
-                    ListaEmpleadosVigentes.append(E)
-            else:
-                pass
-        ArchivoEmpleado.close()
-        ArchivoEB.close()
-        return(ListaEmpleadosVigentes) 
+    for E in ListaEmpleados:
+        R = (E.split("|"))
+        N = R[0]
+        for S in FINAL:
+            if N == S:
+                ListaEmpleadosVigentes.append(E)
+        else:
+            pass
+    ArchivoEmpleado.close()
+    ArchivoEB.close()
+    return(ListaEmpleadosVigentes) 
 
-def replace_line(file_name, line_num, text):
-        lines = open(file_name, 'r').readlines()
-        lines[line_num] = text
-        out = open(file_name, 'w')
-        out.writelines(lines)
-        out.close()
+def BorrarLinea(Archivo, Indice, Texto):
+    ##SE BORRA 1 LINEA DE UN ARCHIVO##
+    lines = open(Archivo, 'r').readlines()
+    lines[Indice] = Texto
+    out = open(Archivo, 'w')
+    out.writelines(lines)
+    out.close()
 
 class Empleado:
     def __init__ (self,idempleado,nombre,direccion):
@@ -63,6 +67,7 @@ class Empleado:
     
     @classmethod
     def OpciondeEmpleado(self,opcion):
+        ##AGREGAR##
         if opcion == 1:
             Lista = []
             contador = 1
@@ -107,7 +112,7 @@ class Empleado:
             NombreN = input("Dame el nombre nuevo que le quieres poner:")
             DireccionN = input("Dame la direccion nueva que le quieres poner:")
             Indice = EmpledoM - 1
-            replace_line("./archivos/Empleado.txt", Indice, f"{EmpledoM}|{NombreN}|{DireccionN}\n")
+            BorrarLinea("./archivos/Empleado.txt", Indice, f"{EmpledoM}|{NombreN}|{DireccionN}\n")
             limpiar()
         ##CONSULTA ESPECIFICA##
         elif opcion == 5:
@@ -115,48 +120,28 @@ class Empleado:
             limpiar()
             ConsultaSRT = str(ConsultaEspecifica)
             Actualizacion2 = (EmpleadoVigente())
-            print("-"*40)
+            print("-----------------------------------")
             for Z in Actualizacion2:
-                A = (Z[0:1])
+                O = Z.split("|")
+                A = O[0]
                 if A == ConsultaSRT:
                     print(Z)
-            print("-"*40)
             
         elif opcion == 4:
+            ##CONSULTAR TODOS##
             Actualizacion3 = (EmpleadoVigente())
             ArchivoEmpleado = open("./Archivos/Empleado.txt","r",encoding="utf8")
             limpiar()
+            print("-----------------------------------")
             ListaEmpleados = (ArchivoEmpleado.read().splitlines())
-            print("-"*40)
-    
+            
             for F in ListaEmpleados:
-                A = (F[0:1])
-                for M in Actualizacion3:
-                    R = (M[0:1])
-                    if A == R:
+                B = F.split("|")
+                A = B[0]
+                for J in Actualizacion3:
+                    S = J.split("|")
+                    M = S[0]
+                    if A == M:
                         print(F)
                     else:
                         pass
-            print("-"*40)
-            
-                
-                
-
-
-
-
-
-
-
-
-
-
-            
-
-
-
-
-
-
-        
-
