d08e19584f0bbbbff9dba25813239cc19b6b6257
Miky235 <milo_rayo@hotmail.com>
2020-05-26 21:55:31 -0500

    Arregle mi codigo, y lo hice mas bonito

--- temas.py
+++ temas.py
@@ -13,23 +13,87 @@ class TEMAS:
         self.__nombre = valor
     
     def AgregarTema(self):
-        archivo = open('./archivos/Temas.txt','w',encoding='utf8')
+        archivo = open('./archivos/Temas.txt','a',encoding='utf8')
         archivo.write(self.__idtema + '|' + self.__nombre  + '\n')
         archivo.close()
-#--------------------------------------------------
+
     @classmethod
-    def ConsultarTodo(self,archivo):
-        archivo1 = open(archivo, encoding='utf8')
-        print(archivo1.read())
+    def Agregar(self,ubi,ubi2,contador):
+        nombre = input('Nombre: ')
+        Lista = []
+        archivo1 = open(ubi2, encoding='utf8')
+        cont = 6
+        for n in archivo1:
+            dato = n.split('\n')
+            contador = contador - 1
+            cont = cont - 1
+            Lista.append(dato[0])
+            if contador == 0:
+                idtema = str(int(dato[0]) + 1)
+                Lista.remove(dato[0])
+                Lista.append(idtema)
+            archivo2 = open(ubi2,"w",encoding = "utf8")
+            for a in Lista:
+                archivo2.write(a + '\n')
+            archivo2.close()
+            if cont == 0:
+                break
+        archivo1.close()
+        T = TEMAS(idtema,nombre)
+        T.AgregarTema()
+#--------------------------------------------------
+   @classmethod
+    def Eliminar(self,ubi,clave):
+        Lista = []
+        archivo1 = open(ubi, encoding='utf8')
+        for n in archivo1:
+            Lista.append(n)
+            if clave == n[0]:
+                Lista.remove(n)
+            archivo2 = open(ubi,"w",encoding = "utf8")
+            for g in Lista:
+                archivo2.write(g)
+            archivo2.close()
         archivo1.close()
 
 #--------------------------------------------------
-    @classmethod
-    def ConsultaEspecifica(self,archivo,clave):
-        archivo1 = open(archivo, encoding='utf8')
+   @classmethod
+    def EditarTema(self,ubi,idt):
         Lista = []
         Numeros = ['0','1','2','3','4','5','6','7','8','9']
+        archivo1 = open(ubi, encoding='utf8')
         for n in archivo1:
+            Lista.append(n)
+            if idt == n[0] and n[1] not in Numeros:
+                if idt == n[0]:
+                    Nombre = input('Nombre nuevo: ')
+                    T = idt + '|' + Nombre + '\n'
+                    Lista.remove(n)
+                    Lista.append(T)
+            elif idt == (n[0] + n[1]):
+                Nombre = input('Nombre nuevo: ')
+                T = idt + '|' + Nombre + '\n'
+                Lista.remove(n)
+                Lista.append(T)
+            archivos2 = open(ubi,"w",encoding = "utf8")                
+            for x in Lista:
+                archivos2.write(x)       
+            archivos2.close()
+        archivo1.close()
+#---------------------------------------------------
+    @classmethod
+    def ConsultarTodo(self,ubi):
+        archivo = open(ubi, encoding='utf8')
+        print(archivo.read())
+        archivo.close()
+
+#----------------------------------------------------
+    @classmethod
+    def ConsultaEspecifica(self,ubi,clave):
+        archivo = open(ubi, encoding='utf8')
+        Lista = []
+        Numeros = ['0','1','2','3','4','5','6','7','8','9']
+        for n in archivo:
             Lista.append(n)
             if clave == n[0] and n[1] not in Numeros:
                 if clave == n[0]:
@@ -37,21 +101,7 @@ class TEMAS:
             elif len(clave) == len(n[0] + n[1]):
                 if clave == (n[0] + n[1]):
                     print(n)
-        archivo1.close()
-#---------------------------------------------------
-    @classmethod
-    def Eliminar(self,archivo,clave):
-        Lista = []
-        archivo1 = open(archivo, encoding='utf8')
-        for n in archivo1:
-            Lista.append(n)
-            if clave == n[0]:
-                Lista.remove(n)
-            archivo2 = open(archivo,"w",encoding = "utf8")
-            for g in Lista:
-                archivo2.write(g)
-            archivo2.close()
-        archivo1.close()
+        archivo.close()
 
 
     
