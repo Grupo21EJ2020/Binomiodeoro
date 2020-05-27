c0a8a9d473a2a87434ae58b543974a97a389941e
diegoalz <diego.lunaza@uanl.edu.mx>
2020-05-26 23:04:37 -0500

    correccion de funcion

--- curso_tema.py
+++ curso_tema.py
@@ -25,10 +25,12 @@ class Curso_Tema:
     def idtema(self, valor):
         self.__idcurso = valor
 
+    def __eq__(self, otro):
+        return self.__idct == otro.__idct
 
     def agregarct(self):
-        archivo = open('./archivos/curso_tema.txt', 'a', encoding='utf8')
-        archivo.write(f"{'ID_Curso_Tema: %s'%self.__idct}, {'ID_Curso: %s'%self.idcurso} {'ID_Tema: %s'%self.__idtema} \n")
+        archivo = open('./archivos/curso_tema.txt', 'w', encoding='utf8')
+        archivo.write(f"{'ID_Curso_Tema: %s'%self.__idct} {'ID_Curso: %s'%self.idcurso} {'ID_Tema: %s'%self.__idtema} \n")
         archivo.close()
     
     def borrarct(self, valor):
@@ -44,18 +46,22 @@ class Curso_Tema:
     def modificarct(self, valor, valor2, valor3):
         archivo = open('./archivos/curso_tema.txt', 'r')
         clave = archivo.readlines()
-
+    
         for i in clave:
             if valor in i:
-                archivo = open('./archivos/curso_tema.txt', 'w')
-                clave.remove(i) 
-                clave = (f"ID_Curso_Tema: {valor}  ID_Curso: {valor2}  ID_Tema: {valor3} \n") + "%s"%clave
+                clave.remove(i)
+        
+        clave = " ".join(clave)
+        archivo = open('./archivos/curso_tema.txt', 'w', encoding='utf8')
+        clave = (f"ID_Curso_Tema: {valor}  ID_Curso: {valor2}  ID_Tema: {valor3} \n" + "%s"%clave)
         archivo.write("%s"%clave)
         archivo.close
+    
 
-primero = Curso_Tema("10", "500", "35")
+'''primero = Curso_Tema("10", "500", "35")
 segundo = Curso_Tema("15", "400", "40")
 
 primero.agregarct()
+segundo.agregarct()
 
-primero.modificarct("10", "400", "36")
+primero.modificarct("10", "400", "600")'''
