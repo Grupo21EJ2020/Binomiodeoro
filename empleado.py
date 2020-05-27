0d185b4f15e21150c4db0ff3dd0da8f1cc937093
EdgarZamoraa <zamorasaucedoedgar@gmail.com>
2020-05-26 19:23:26 -0500

    Se le avanzo ala opcion de modificar en empleado

--- empleado.py
+++ empleado.py
@@ -25,6 +25,38 @@ class Empleado:
     #Modificar
     #Info de todos
     #Cosulta individual
+def EmpleadoVigente():
+    ArchivoEmpleado = open("./Archivos/Empleado.txt","r",encoding="utf8")
+    ArchivoEB = open("./archivos/EmpleadoBorrado.txt","r",encoding="utf8")
+
+    ListaEmpleados = (ArchivoEmpleado.read().splitlines())
+    ListaBorrados = (ArchivoEB.read().splitlines())
+
+    ListaEmpleadosVigentes = []
+
+    IDEmpleados = []
+    for E in ListaEmpleados:
+        N = (E[0:1])
+        IDEmpleados.append(N)
+
+    IDEmpleadoSET = set(IDEmpleados)
+    ListaEmpleadosSET = set(ListaBorrados)
+
+    final = IDEmpleadoSET - ListaEmpleadosSET
+    FINAL = list(final)
+
+
+
+    for E in ListaEmpleados:
+        N = (E[0:1])
+        for S in FINAL:
+            if N == S:
+                ListaEmpleadosVigentes.append(E)
+        else:
+            pass
+    ArchivoEmpleado.close()
+    ArchivoEB.close()
+    return(ListaEmpleadosVigentes)    
 
 
     
@@ -46,6 +78,7 @@ def SubMenuEmpleado(opcion):
             ArchivoEmpleado.write(f"{ID}|{nombre}|{direccion}\n")
             
             ArchivoEmpleado.close()
+
         elif opcion == 2:
             IDborrarINT = int(input("Dame la id del empledo que quieres borrar"))
             IDborrarSTR = str(IDborrarINT)
@@ -53,66 +86,24 @@ def SubMenuEmpleado(opcion):
             ArchivoEB = open("./archivos/EmpleadoBorrado.txt","a",encoding="utf8")
             ArchivoEB.write(f"{IDborrarSTR}\n")
             ArchivoEB.close()
-
-            ArchivoEmpleado = open("./Archivos/Empleado.txt","r",encoding="utf8")
-            ArchivoEB = open("./archivos/EmpleadoBorrado.txt","r",encoding="utf8")
-
-            ListaEmpleados = (ArchivoEmpleado.read().splitlines())
-            ListaBorrados = (ArchivoEB.read().splitlines())
-
-            ListaEmpleadosVigentes = []
-
-            IDEmpleados = []
-            for E in ListaEmpleados:
-                N = (E[0:1])
-                IDEmpleados.append(N)
-
-            IDEmpleadoSET = set(IDEmpleados)
-            ListaEmpleadosSET = set(ListaBorrados)
-
-            final = IDEmpleadoSET - ListaEmpleadosSET
-            FINAL = list(final)
-
-
-
-            for E in ListaEmpleados:
-                N = (E[0:1])
-                for S in FINAL:
-                    if N == S:
-                        ListaEmpleadosVigentes.append(E)
-                else:
-                    pass
-            ArchivoEmpleado.close()
-            ArchivoEB.close()
-            print(ListaEmpleadosVigentes)
+            Actualizacion1 = (EmpleadoVigente())
+            print(yes)
             print("##Empleado borrado##")
-
-
-
-
-
+        ## MODIFICAR ##
+        elif opcion == 3:
+            EmpledoM = int(input("Dame el ID del empleado que quieres modificar"))
+            EmpledoMSTR = str(EmpledoM)
+            Actualizacion2 = (EmpleadoVigente())
+            EmpleadoAModificar = []
             
+            for Ac2 in Actualizacion2:
+                N = (Ac2[0:1])
+                if N == EmpledoMSTR:
+                    EmpleadoAModificar.append(Ac2)
 
-                        
-                
+            print(EmpleadoAModificar)
 
-
-                
-
-            
-
-            
-
-            
-                    
-
-
-
-                
-
-            
-
-opcion = 2
+opcion = 3
 SubMenuEmpleado(opcion)
 
 
