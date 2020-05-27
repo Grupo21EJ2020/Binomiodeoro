430e0b5ebe67be485eab5c29aa1bba822799e309
Thomas Medina <thomasmedina397@gmail.com>
2020-05-27 03:51:42 -0500

    Se creo la clase Curso con sus propiedades y setters

---
+++ cursos.py
@@ -0,0 +1,32 @@
+class Curso:
+    def __init__ (self, id_curso, descripcion, id_empleado):
+        self.__id_curso = id_curso
+        self.__descripcion = descripcion 
+        self.__id_empleado = id_empleado
+ 
+        
+    #Privatizacion de atributos de la clase Curso
+    @property
+    def id_curso(self):
+        return self.__id_empleado
+    
+    @property
+    def descripcion(self):
+        return self.__descripcion
+    
+    @property
+    def id_empleado(self):
+        return self.__id_empleado
+    
+    #Se crean los setter de la clase Curso
+    @id_curso.setter
+    def id_curso(self,valor):
+        self.__id_curso = valor
+    
+    @descripcion.setter
+    def descripcion(self, valor):
+        self.__descripcion = valor
+
+    @id_empleado.setter
+    def id_empleado(self, valor):
+        self.__id_empleado = valor
