f716685bdb809c94f64541b18d017f43ba3be0d9
diegoalz <diego.lunaza@uanl.edu.mx>
2020-05-27 02:59:30 -0500

    Correccion a menu añadido

--- curso_tema.py
+++ curso_tema.py
@@ -32,39 +32,7 @@ class Curso_Tema:
         archivo = open('./archivos/curso_tema.txt', 'a', encoding='utf8')
         archivo.write(f"{'ID_Curso_Tema: %s'%self.__idct} | {'ID_Curso: %s'%self.idcurso} | {'ID_Tema: %s'%self.__idtema} \n")
         archivo.close()
-    
-    def borrarct(self, valor):
-        archivo = open('./archivos/curso_tema.txt', 'r')
-        clave = archivo.readlines()
-        for i in clave:
-            if valor in i:
-                clave.remove(i)
-        archivo = open('./archivos/curso_tema.txt', 'w')
-        clave = " ".join(clave)
-        archivo.write("%s"%clave)
-        archivo.close
-    
-    def modificarct(self, valor, valor2, valor3):
-        archivo = open('./archivos/curso_tema.txt', 'r')
-        clave = archivo.readlines()
-    
-        for i in clave:
-            if valor in i:
-                clave.remove(i)
-        
-        clave = " ".join(clave)
-        archivo = open('./archivos/curso_tema.txt', 'w', encoding='utf8')
-        clave = (f"ID_Curso_Tema: {valor} | ID_Curso: {valor2} | ID_Tema: {valor3} \n" + "%s"%clave)
-        archivo.write("%s"%clave)
-        archivo.close
-    
-    def busqueda(self, valor):
-        archivo = open('./archivos/curso_tema.txt', 'r')
-        clave = archivo.readlines()
-        for i in clave:
-            if valor in i:
-                return i
-    
+       
 #Methods inicializadores para el static
     @classmethod
     def agregarI(self, contador):
